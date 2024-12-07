3ec7c6be9412d68a1a66b335b351dcacd3464f70
shoen1x <mohdmuizz22@yahoo.com>
2024-12-08 00:28:25 +0800

    fix: :bug: root mismatch fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shoen1x.github.io',
   integrations: [tailwind({ applyBaseStyles: false })],
   vite: {
     build: {
