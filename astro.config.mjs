0f93b46fcca5427b18a20419039be3fb348b2974
shoen1x <mohdmuizz22@yahoo.com>
2024-12-08 00:52:30 +0800

    fix: :bug: bundling changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: 'entry.[hash].mjs',
-          chunkFileNames: 'chunks/chunk.[hash].mjs',
-          assetFileNames: 'assets/asset.[hash][extname]',
-        },
-      },
-    },
-  },
+  integrations: [tailwind({ applyBaseStyles: false })]
 });
