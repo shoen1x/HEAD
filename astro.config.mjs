9a730d828c122a7003cba1d0526b2c2ee1e377e1
shoen1x <mohdmuizz22@yahoo.com>
2024-12-08 01:26:26 +0800

    chore: generic build filenames integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import genericBuildFilenames from 'astro-generic-build-filenames';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })]
+  site: "https://shoen1x.github.io",
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    genericBuildFilenames()
+  ]
 });
