59878d85c85f5a45d96281d48381a66e7774a4d7
shoen1x <mohdmuizz22@yahoo.com>
2024-12-08 01:32:28 +0800

    chore: adjustment in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import genericBuildFilenames from 'astro-generic-build-filenames';
 // https://astro.build/config
 export default defineConfig({
   site: "https://shoen1x.github.io",
+  base: "HEAD",
   integrations: [
     tailwind({ applyBaseStyles: false }),
     genericBuildFilenames()
