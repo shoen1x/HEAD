c9eda31cece52d93902183f5d6bf33c673307d02
shoen1x <mohdmuizz22@yahoo.com>
2024-12-08 00:15:19 +0800

    fix: :bug: css fixes
    possible fixes for css not appearing using tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shoen1x.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind({ applyBaseStyles: false })]
 });
