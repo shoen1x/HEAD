855d5fd5fbeafef6ff7565cab636824290a6de7c
shoen1x <mohdmuizz22@yahoo.com>
2024-12-08 00:21:33 +0800

    fix: :bug: rollup export potential fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,16 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shoen1x.github.io',
-  integrations: [tailwind({ applyBaseStyles: false })]
+  integrations: [tailwind({ applyBaseStyles: false })],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'entry.[hash].mjs',
+          chunkFileNames: 'chunks/chunk.[hash].mjs',
+          assetFileNames: 'assets/asset.[hash][extname]',
+        },
+      },
+    },
+  },
 });
