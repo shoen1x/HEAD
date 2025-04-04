3d4b8364f989b0d1f6bf075e1513b77dae6860cb
shoen1x <mohdmuizz22@yahoo.com>
2025-04-04 23:12:58 +0800

    feat: :art: Major page overhaul

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://shoen1x.github.io",
   base: "HEAD",
-  integrations: [tailwind({ applyBaseStyles: false }), mdx(), svelte()],
+  integrations: [mdx(), svelte()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'tokyo-night',
+    },
+  },
 });
---
+++ src/kandungan/data/MinDisyorkan.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<div class="relative flex justify-center items-center flex-col w-full h-full gap-4 bg-black/25 backdrop-blur-3xl p-12 rounded-lg">
+    <h1>
+      Minimum yang disyorkan
+    </h1>
+    <h2>
+      Berikut adalah elemen penting untuk mana-mana dokumen web (laman web/apl):
+    </h2>
+    <pre class="bg-[#20405c] w-full h-full inline-block whitespace-pre-line *:whitespace-pre-wrap">
+        <code>&lt;meta charset="utf-8" /&gt;</code>
+        <code>&lt;meta name="viewport" content="width=device-width, initial-scale=1" /&gt;</code>
+        <code>
+            &lt;!--
+                2 meta tag di atas *mesti* didahulukan di &lt;head&gt;
+                untuk memastikan penyampaian dokumen yang betul secara konsisten.
+                Mana-mana elemen &lt;head&gt; lain harus datang *selepas* tag ini.
+            --&gt;
+        </code>
+    </pre>
+    <p class="block relative w-full h-full whitespace-pre-wrap">
+      <b>`meta charset`</b> - mentakrifkan pengekodan (encoding) laman web, `utf-8` ialah standard
+
+      <b>`meta name="viewport"`</b> - tetapan viewport yang berkaitan dengan responsif antara muka mudah alih
+
+      <b>`width=device-width`</b> - gunakan lebar fizikal peranti (bagus untuk antara muka mudah alih!)
+
+      <b>`initial-scale=1`</b> - zum permulaan, 1 bermaksud tiada zum
+    </p>
+  </div>
